89ea4524c7b05afc4ccbef9064d522f8c40eaaaa
PauloPortofolio <pvba02@gmail.com>
2022-11-04 11:35:56 -0400

    15.0) Stream Operations with Lambda
    -> CONCLUDED:
    REFACTORING reductions
    
    -> WORKING:
    
    -> NEXT STEPS:
    
    -> FUTURE IMPLEMENTATIONS:

--- src/main/java/reduction/Reduce_Identity.java
+++ src/main/java/reduction/Reduce_Identity.java
@@ -2,28 +2,59 @@ package reduction;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+
+import static java.lang.String.join;
 
 public class Reduce_Identity {
   private static void writeRandomNumbersToFile(String fileName) throws IOException {
 
     Random r = new Random();
-    String numbersList;
+    String strList;
+    List<Integer> numbers = Arrays.asList(1, 2, 3);
 
     try (FileWriter stream = new FileWriter(fileName)) {
-      for (int i = 0; i < 10; i++) {
-        numbersList = r.ints(7, 1, 9)
-                       .mapToObj((s) -> String.valueOf(s))
-                       .reduce("->", (string, element) -> String.join(" <> ", string, element));
-        System.out.println(numbersList);
-        stream.write(numbersList + "\n");
-      }
+
+      strList =
+           numbers
+                .stream()
+                .map(x -> x.toString())
+                .reduce(
+                     "->",
+                     (letters, letterToAdd) ->
+                          letters +"$"+ letterToAdd
+                );
+      show(strList);
+      stream.write(strList + "\n");
+
     }
+
+    numbers = Arrays.asList(1, 2, 3);
+    int startValue = 1;
+
+    show(numbers.toString());
+    int result =
+         numbers
+              .stream()
+              .reduce(
+                   startValue,
+                   (subtotal, totalizedItem) -> {
+                     return subtotal + totalizedItem; //Integer::sum
+                   }
+              );
+    show(String.valueOf(result));
   }
 
   public static void main(String[] args) throws IOException {
 
     writeRandomNumbersToFile("characterData.txt");
 
+  }
+
+  private static void show(String text) {
+
+    System.out.println(text);
   }
 }
