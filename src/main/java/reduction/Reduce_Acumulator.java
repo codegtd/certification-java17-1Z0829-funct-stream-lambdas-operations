5636f54d8cdde36127c01846302c94844fc0a98f
PauloPortofolio <pvba02@gmail.com>
2022-11-04 11:01:01 -0400

    14.0) Stream Operations with Lambda
    -> CONCLUDED:
    REFACTORING reductions
    
    -> WORKING:
    
    -> NEXT STEPS:
    
    -> FUTURE IMPLEMENTATIONS:

--- src/main/java/reduction/Reduce_Acumulator.java
+++ src/main/java/reduction/Reduce_Acumulator.java
@@ -8,20 +8,18 @@ public class Reduce_Acumulator {
   public static void main(String[] args) {
 
     List<Integer> list = Arrays.asList(1, 2, 3, 4);
-
     final Optional<Integer> reduction =
-         list
-              .stream()
-              .reduce((subtotal, cumulate_in_subtotal) -> {
-show("subtotal: " + subtotal + " - Cumulator: " + cumulate_in_subtotal);
-                // Associate Acumulation: Function cumulating in SubTotal
-                return subtotal + cumulate_in_subtotal;
-              });
+         list.stream()
+             .reduce((subtotal, cumulate_in_subtotal) -> {
+
+               show("subtotal: " + subtotal +
+                         " - Cumulator: " + cumulate_in_subtotal);
+
+       // Associate Acumulation: Function cumulating in SubTotal
+               return subtotal + cumulate_in_subtotal;    });
 
     final Integer intRed = reduction.get();
-
     show("Reduction = " + intRed);
-
   }
 
   private static void show(String text) {
